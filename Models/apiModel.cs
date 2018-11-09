ee87eabdaa9c753e95eb08663545dc739a68bdda
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-11-09 15:56:16 -0600

    adding api support

---
+++ Models/apiModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace trill.Models {
+    public class apiModel {
+        public string leaderboard{get;set;}
+
+        public string api_key {get;set;}
+    }
+}
