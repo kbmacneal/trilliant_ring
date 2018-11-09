323f9087224a7d7e973fcde03af7eca5f80b483b
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-11-09 15:59:37 -0600

    finalizing support

---
+++ Controllers/LeaderboardController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using trill.Classes;
+using trill.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace trill.Controllers {
+    public class SocialController : Controller {
+
+        [HttpGet]
+        public ActionResult Index (apiModel model) {
+            model.leaderboard = Controllers.stats.get_stats().leaderboard.Replace(System.Environment.NewLine, "<br />");
+            return View (model);
+        }
+    }
+}
