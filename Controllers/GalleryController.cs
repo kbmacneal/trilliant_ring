9856ea781c40e7b48b214f7f5883cc6c295e300b
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-11-12 09:53:37 -0600

    adopting more fluid design

--- Controllers/GalleryController.cs
+++ Controllers/GalleryController.cs
@@ -21,23 +21,12 @@ namespace trill.Controllers {
 
             List<string> raw_html = new List<string> ();
 
-            while (filenames.Count > 0) {
-                raw_html.Add ("<div class=\"row row-fluid mb-4\">");
+            KeyValuePair<string, string>[] elements = filenames.ToArray ();
 
-                int count = filenames.Count () >= 3 ? 3 : filenames.Count ();
-
-                KeyValuePair<string, string>[] elements = filenames.Take (count).ToArray ();
-
-                foreach (KeyValuePair<string, string> element in elements) {
-                    string add = "<div class=\"col-md-4\"><div class=\"card\"><img class=\"img-scaled\" src=\"<element>\" style=\"max-height:200px;max-width:200px;\"></div></div>";
-                    raw_html.Add (add.Replace ("<element>", element.Value));
-                    filenames.Remove (element.Key);
-                }
-
-                raw_html.Add ("</div>");
-
-                // filenames.RemoveRange(0,count);
-
+            foreach (KeyValuePair<string, string> element in elements) {
+                string add = "<img class=\"photos w-25\" src=\"<element>\" />";
+                raw_html.Add (add.Replace ("<element>", element.Value));
+                filenames.Remove (element.Key);
             }
 
             model.row_html = String.Join (System.Environment.NewLine, raw_html);
@@ -45,6 +34,37 @@ namespace trill.Controllers {
             return View ("Gallery", model);
         }
         public IActionResult Flamingo (GalleryModel model) {
+            // List<string> files = System.IO.Directory.GetFiles ("wwwroot/Assets/FlamingoStuff/").ToList ();
+
+            // string prepend = "https://trilliantring.com/Assets/FlamingoStuff/";
+
+            // SortedDictionary<string, string> filenames = new SortedDictionary<string, string> ();
+
+            // files.ForEach (e => filenames.Add (System.IO.Path.GetFileName (e), String.Concat (prepend, System.IO.Path.GetFileName (e))));
+
+            // List<string> raw_html = new List<string> ();
+
+            // while (filenames.Count > 0) {
+            //     raw_html.Add ("<div class=\"row row-fluid mb-4\">");
+
+            //     int count = filenames.Count () >= 3 ? 3 : filenames.Count ();
+
+            //     KeyValuePair<string, string>[] elements = filenames.Take (count).ToArray ();
+
+            //     foreach (KeyValuePair<string, string> element in elements) {
+            //         string add = "<div class=\"col-md-4\"><div class=\"card\"><img class=\"img-scaled\" src=\"<element>\" style=\"max-height:200px;max-width:200px;\"></div></div>";
+            //         raw_html.Add (add.Replace ("<element>", element.Value));
+            //         filenames.Remove (element.Key);
+            //     }
+
+            //     raw_html.Add ("</div>");
+
+            //     // filenames.RemoveRange(0,count);
+
+            // }
+
+            // model.row_html = String.Join (System.Environment.NewLine, raw_html);
+
             List<string> files = System.IO.Directory.GetFiles ("wwwroot/Assets/FlamingoStuff/").ToList ();
 
             string prepend = "https://trilliantring.com/Assets/FlamingoStuff/";
@@ -55,23 +75,12 @@ namespace trill.Controllers {
 
             List<string> raw_html = new List<string> ();
 
-            while (filenames.Count > 0) {
-                raw_html.Add ("<div class=\"row row-fluid mb-4\">");
+            KeyValuePair<string, string>[] elements = filenames.ToArray ();
 
-                int count = filenames.Count () >= 3 ? 3 : filenames.Count ();
-
-                KeyValuePair<string, string>[] elements = filenames.Take (count).ToArray ();
-
-                foreach (KeyValuePair<string, string> element in elements) {
-                    string add = "<div class=\"col-md-4\"><div class=\"card\"><img class=\"img-scaled\" src=\"<element>\" style=\"max-height:200px;max-width:200px;\"></div></div>";
-                    raw_html.Add (add.Replace ("<element>", element.Value));
-                    filenames.Remove (element.Key);
-                }
-
-                raw_html.Add ("</div>");
-
-                // filenames.RemoveRange(0,count);
-
+            foreach (KeyValuePair<string, string> element in elements) {
+                string add = "<img class=\"photos w-25\" src=\"<element>\" />";
+                raw_html.Add (add.Replace ("<element>", element.Value));
+                filenames.Remove (element.Key);
             }
 
             model.row_html = String.Join (System.Environment.NewLine, raw_html);
@@ -90,23 +99,12 @@ namespace trill.Controllers {
 
             List<string> raw_html = new List<string> ();
 
-            while (filenames.Count > 0) {
-                raw_html.Add ("<div class=\"row row-fluid mb-4\">");
+            KeyValuePair<string, string>[] elements = filenames.ToArray ();
 
-                int count = filenames.Count () >= 3 ? 3 : filenames.Count ();
-
-                KeyValuePair<string, string>[] elements = filenames.Take (count).ToArray ();
-
-                foreach (KeyValuePair<string, string> element in elements) {
-                    string add = "<div class=\"col-md-4\"><div class=\"card\"><img class=\"img-scaled\" src=\"<element>\" style=\"max-height:200px;max-width:200px;\"></div></div>";
-                    raw_html.Add (add.Replace ("<element>", element.Value));
-                    filenames.Remove (element.Key);
-                }
-
-                raw_html.Add ("</div>");
-
-                // filenames.RemoveRange(0,count);
-
+            foreach (KeyValuePair<string, string> element in elements) {
+                string add = "<img class=\"photos w-25\" src=\"<element>\" />";
+                raw_html.Add (add.Replace ("<element>", element.Value));
+                filenames.Remove (element.Key);
             }
 
             model.row_html = String.Join (System.Environment.NewLine, raw_html);
@@ -125,23 +123,12 @@ namespace trill.Controllers {
 
             List<string> raw_html = new List<string> ();
 
-            while (filenames.Count > 0) {
-                raw_html.Add ("<div class=\"row row-fluid mb-4\">");
+            KeyValuePair<string, string>[] elements = filenames.ToArray ();
 
-                int count = filenames.Count () >= 3 ? 3 : filenames.Count ();
-
-                KeyValuePair<string, string>[] elements = filenames.Take (count).ToArray ();
-
-                foreach (KeyValuePair<string, string> element in elements) {
-                    string add = "<div class=\"col-md-4\"><div class=\"card\"><img class=\"img-scaled\" src=\"<element>\" style=\"max-height:200px;max-width:200px;\"></div></div>";
-                    raw_html.Add (add.Replace ("<element>", element.Value));
-                    filenames.Remove (element.Key);
-                }
-
-                raw_html.Add ("</div>");
-
-                // filenames.RemoveRange(0,count);
-
+            foreach (KeyValuePair<string, string> element in elements) {
+                string add = "<img class=\"photos w-25\" src=\"<element>\" />";
+                raw_html.Add (add.Replace ("<element>", element.Value));
+                filenames.Remove (element.Key);
             }
 
             model.row_html = String.Join (System.Environment.NewLine, raw_html);
@@ -160,23 +147,12 @@ namespace trill.Controllers {
 
             List<string> raw_html = new List<string> ();
 
-            while (filenames.Count > 0) {
-                raw_html.Add ("<div class=\"row row-fluid mb-4\">");
+            KeyValuePair<string, string>[] elements = filenames.ToArray ();
 
-                int count = filenames.Count () >= 3 ? 3 : filenames.Count ();
-
-                KeyValuePair<string, string>[] elements = filenames.Take (count).ToArray ();
-
-                foreach (KeyValuePair<string, string> element in elements) {
-                    string add = "<div class=\"col-md-4\"><div class=\"card\"><img class=\"img-scaled\" src=\"<element>\" style=\"max-height:200px;max-width:200px;align-self:center;\"></div></div>";
-                    raw_html.Add (add.Replace ("<element>", element.Value));
-                    filenames.Remove (element.Key);
-                }
-
-                raw_html.Add ("</div>");
-
-                // filenames.RemoveRange(0,count);
-
+            foreach (KeyValuePair<string, string> element in elements) {
+                string add = "<img class=\"photos w-25\" src=\"<element>\" />";
+                raw_html.Add (add.Replace ("<element>", element.Value));
+                filenames.Remove (element.Key);
             }
 
             model.row_html = String.Join (System.Environment.NewLine, raw_html);
