196830c87b501c67078551dfa130166eae90ab46
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-11-20 09:14:33 -0600

    making display less opaque

--- Controllers/LeaderboardController.cs
+++ Controllers/LeaderboardController.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 using trill.Classes;
 using trill.Models;
 using Microsoft.AspNetCore.Mvc;
+using Westwind.Utilities;
 
 namespace trill.Controllers {
     public class LeaderboardController : Controller {
 
         [HttpGet]
         public ActionResult Index (apiModel model) {
-            model.leaderboard = Controllers.stats.get_stats().leaderboard.Replace(System.Environment.NewLine, "<br />");
+            model.leaderboard = HtmlUtils.DisplayMemo(Controllers.stats.get_stats().leaderboard);
             return View (model);
         }
     }
