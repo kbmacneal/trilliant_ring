423e4fbf9c98f79d677f146f0113321f269d6e45
bowmorek <kenneth.b.macneal@gmail.com>
2019-11-13 15:54:31 -0600

    updating to dotnet core 3 language

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Hosting;
 
 namespace trill {
     public class Startup {
@@ -31,7 +32,7 @@ namespace trill {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public static void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+        public static void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
             } else {
